27d9238bb550d16a28d7ee742c5c9992430a1686
Martin Letis <mletis@google.com>
2023-04-12 12:40:10 +1000

    Revert "Use args[0]"
    This reverts commit db6a76d1cb1e13054f2b50c0cc71b740f6566bb8.

--- src/main/java/com/martinletis/photos/PhotoManager.java
+++ src/main/java/com/martinletis/photos/PhotoManager.java
@@ -2,14 +2,14 @@ package com.martinletis.photos;
 
 import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AbstractPromptReceiver;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleOAuthConstants;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.Credentials;
@@ -25,19 +25,24 @@ import com.google.photos.types.proto.Album;
 import com.google.photos.types.proto.MediaItem;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.util.Collections;
 
 public class PhotoManager {
-
   private static final String APP_NAME = "martinletis-photos-0.1";
+  private static final String CLIENT_SECRET =
+      "client_secret_575230163-c5oce6lk0e14hgmlp6shh0rbkaacektr.apps.googleusercontent.com.json";
 
   public static void main(String[] args) throws Exception {
     NetHttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
-    JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+    JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
 
     GoogleClientSecrets secrets;
-    try (Reader reader = new FileReader(args[0])) {
+    try (Reader reader =
+        new FileReader(
+            Joiner.on(File.separator)
+                .join(System.getProperty("user.home"), "tmp", CLIENT_SECRET))) {
       secrets = GoogleClientSecrets.load(jsonFactory, reader);
     }
 
@@ -52,11 +57,20 @@ public class PhotoManager {
                 jsonFactory,
                 secrets,
                 Collections.singleton("https://www.googleapis.com/auth/photoslibrary.readonly"))
+            .setAccessType("offline")
             .setDataStoreFactory(new FileDataStoreFactory(dataDirectory))
             .build();
 
     Credential credential =
-        new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+        new AuthorizationCodeInstalledApp(
+                flow,
+                new AbstractPromptReceiver() {
+                  @Override
+                  public String getRedirectUri() throws IOException {
+                    return GoogleOAuthConstants.OOB_REDIRECT_URI;
+                  }
+                })
+            .authorize(APP_NAME);
 
     Credentials credentials =
         UserCredentials.newBuilder()
@@ -71,7 +85,7 @@ public class PhotoManager {
             .build();
 
     try (PhotosLibraryClient client = PhotosLibraryClient.initialize(settings)) {
-      if (args.length == 1) {
+      if (args.length == 0) {
         ListAlbumsPagedResponse response =
             client.listAlbums(ListAlbumsRequest.newBuilder().setPageSize(50).build());
         for (Album album : response.iterateAll()) {
