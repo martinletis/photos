3b9cf6fc45df2db5528bca1cdff63309013b8ae4
Martin Letis <mletis@google.com>
2023-04-12 12:20:12 +1000

    Use APP_NAME in datastore

--- src/main/java/com/martinletis/photos/PhotoManager.java
+++ src/main/java/com/martinletis/photos/PhotoManager.java
@@ -43,7 +43,8 @@ public class PhotoManager {
 
     File dataDirectory =
         new File(
-            Joiner.on(File.separator).join(System.getProperty("user.home"), "tmp", "datastore"));
+            Joiner.on(File.separator)
+                .join(System.getProperty("user.home"), "tmp", APP_NAME, "datastore"));
 
     AuthorizationCodeFlow flow =
         new GoogleAuthorizationCodeFlow.Builder(
